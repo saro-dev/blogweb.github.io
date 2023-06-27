1f41105314851a00e4d41512360d3ce54e61944d
Saravanan S <codersaro@gmail.com>
2023-06-27 23:21:27 +0530

    Update new.js

--- new.js
+++ new.js
@@ -1,6 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    if (window.location.pathname.includes(`/blogweb.github.io/?`)) {
+    if (window.location.href.includes(`/blogweb.github.io/`)) {
       const form = document.getElementById('post-form');
       const postsContainer = document.getElementById('posts');
   
@@ -101,7 +101,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   
       renderPosts();
-    } else if (window.location.pathname.includes(`/blogweb.github.io/post.html`)) {
+    } else if (window.location.href.includes(`/blogweb.github.io/post.html`)) {
       const postDetailsContainer = document.getElementById('post-details');
       const deleteButton = document.getElementById('delete-button');
       
